d7dfa94a10a350963febab2d4904757420f10d7c
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-09-29 23:14:02 -0300

    refactor

--- src/services/message/send-message-service.ts
+++ src/services/message/send-message-service.ts
@@ -1,6 +1,15 @@
 import { connectionWhatsapp } from "../../server";
 import { IMessageService } from "./IMessageService";
 
+/**
+ * Por enquanto so envia texto (feat: add type message in request body)
+ * 
+ {
+    "contactNumber": "551100000000@s.whatsapp.net",
+    "textMessage": "sua mensagem"
+}
+ * 
+ */
 export class MessageService implements IMessageService {
   async sendMessage(contactNumber: string, textMessage: string): Promise<any> {
     const whatsapp = connectionWhatsapp.getConnection();
